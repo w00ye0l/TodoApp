545eee3824502101a4847e80faf8730cb596110b
wooyeol <dnduf158@gmail.com>
2022-12-02 01:19:01 +0900

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="todoapp">
+      <Header @insertTodo="insertTodo" />
+      <Todo 
+        :todos="filteredList" 
+        @removeTodo="removeTodo" 
+        @updateDone="updateDone" 
+        @updateTodo="updateTodo"
+      />
+      <Footer 
+        :filterType="filterType" 
+        :size="filteredList.length"
+        @onFilterType="handleFilterType"
+      />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/css/main.css";
+import Header from "./components/Header.vue";
+import Todo from "./components/Todo.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Todo,
+    Footer,
+  },
+  data() {
+    return {
+      todos: [
+      ],
+      filterType: 'All',
+    };
+  },
+  methods: {
+    insertTodo(text) {
+      this.todos = [
+        ...this.todos,
+        {
+          id: new Date().getTime(),
+          text,
+          isDone: false
+        }
+      ];
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    updateDone(id) {
+      const todos = [...this.todos];
+      const todo = todos.find(todo => todo.id === id);
+
+      if (todo) {
+        todo.isDone = !todo.isDone;
+        this.todos = todos;
+      }
+    },
+    updateTodo({ id, text }) {
+      const todos = [...this.todos];
+      const todo = todos.find(todo => todo.id === id);
+
+      if (todo) {
+        todo.text = text;
+        this.todo = todos;
+      }
+    },
+    handleFilterType(type) {
+      this.filterType = type
+    }
+  },
+  computed: {
+    filteredList() {
+      switch(this.filterType) {
+        case "All": {
+          return this.todos
+        }
+        case "Active": {
+          return this.todos.filter((todo) => !todo.isDone)
+        }
+        case "Completed": {
+          return this.todos.filter((todo) => todo.isDone)
+        }
+        default: {
+          return []
+        }
+      }
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
